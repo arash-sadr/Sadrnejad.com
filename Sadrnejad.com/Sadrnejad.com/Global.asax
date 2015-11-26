dc00b028197fb2811759137595b4081632b8792a
arash-sadr <arash.sdr@gmail.com>
2015-11-26 22:13:02 +0800

    Startup changes

---
+++ Sadrnejad.com/Sadrnejad.com/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sadrnejad.com.MvcApplication" Language="C#" %>
---
+++ Sadrnejad.com/Sadrnejad.com/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sadrnejad.com.Startup))]
+namespace Sadrnejad.com
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
