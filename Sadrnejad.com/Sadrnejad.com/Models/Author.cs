b7f38a78a27843a7fae1a15ed16392cc5d284f4d
arash-sadr <arash.sdr@gmail.com>
2015-12-24 11:43:19 +0800

    Adding Model classes Post, Author, Category, Tag and DBContext class

---
+++ Sadrnejad.com/Sadrnejad.com/Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sadrnejad.com.Models
+{
+    public class Author
+    {
+        public virtual int ID { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual string LastName { get; set; }
+        
+
+        public virtual IList<Post> Posts { get; set; }
+    }
+}
---
+++ Sadrnejad.com/Sadrnejad.com/Models/Tag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sadrnejad.com.Models
+{
+    public class Tag
+    {
+        public virtual int ID { get; set; }
+
+        public virtual string Title { get; set; }
+
+        public virtual string Description { get; set; }
+
+        public virtual bool Enabled { get; set; }
+
+        public virtual string UrlSlug { get; set; }
+
+
+        public virtual IList<Post> Posts { get; set; }
+    }
+}
