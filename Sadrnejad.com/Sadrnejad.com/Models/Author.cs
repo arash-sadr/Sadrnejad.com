ed21e4d4a035ef3c730cc66231f5a471a68b46ba
arash-sadr <arash.sdr@gmail.com>
2015-12-25 12:25:42 +0800

    adjust the Model classes to have the foreign key field

--- Sadrnejad.com/Sadrnejad.com/Models/Author.cs
+++ Sadrnejad.com/Sadrnejad.com/Models/Author.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Sadrnejad.com.Models
 {
     public class Author
     {
-        public virtual int ID { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
 
-        public virtual string Name { get; set; }
-
-        public virtual string LastName { get; set; }
-        
-
+        [ForeignKey("Posts")]
+        public int Post_ID { get; set; }
         public virtual IList<Post> Posts { get; set; }
     }
 }
--- Sadrnejad.com/Sadrnejad.com/Models/Category.cs
+++ Sadrnejad.com/Sadrnejad.com/Models/Category.cs
@@ -1,23 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Sadrnejad.com.Models
 {
     public class Category
     {
-        public virtual int ID { get; set; }
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool Enabled { get; set; }
+        public string UrlSlug { get; set; }
 
-        public virtual string Title { get; set; }
-
-        public virtual string Description { get; set; }
-
-        public virtual bool Enabled { get; set; }
-
-        public virtual string UrlSlug { get; set; }
-
-
+        [ForeignKey("Posts")]
+        public int Post_ID { get; set; }
         public virtual IList<Post> Posts { get; set; }
     }
 }
